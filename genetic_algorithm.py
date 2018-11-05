ffc2d06443cd6dd5920e2dc3985e63cd858fee7c
PrismaticPolygon <domwhite97@gmail.com>
2018-11-05 11:57:10 +0000

    WIP: moved GA to OO-implementation

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,79 +1,84 @@
 import numpy as np
+from main import load_file
 
 #Hyperparameters
 population_size = 200
 mutation_probability = 0.01
+num_generations = 100
 selective_pressure = 1.90
+size, distance_matrix = load_file("AISearchtestcase.txt")
+
+class Individual:
+
+    def mutate(self):
+
+        points = [np.random.randint(0, len(self.chromosome), dtype=int) for x in range(2)]
+        low, high = min(points), max(points)
+
+        mutation = self.chromosome[low: high]
+        new_chromosome = np.concatenate((self.chromosome[:low], self.chromosome[high:]))
+        insertion_point = np.random.randint(0, len(new_chromosome), dtype=int)
+
+        self.chromosome =  np.concatenate((new_chromosome[:insertion_point], mutation, new_chromosome[insertion_point:]))
+
+    def __init__(self):
+
+        self.chromosome = self.generate_chromosome()
+        self.cost = self.generate_cost()
+
+    def generate_chromosome(self):
+
+        return np.random.choice(size, size, replace=False) + 1
+
+    def generate_cost(self):
+
+        print(self.chromosome)
+
+        cost = distance_matrix[self.chromosome[0], self.chromosome[-1]]
+
+        for i in range(1, size):
+
+            cost += distance_matrix[self.chromosome[i - 1], self.chromosome[i]]
+
+        return cost
 
 
-def create_population(num_cities, population_size):
-    """
-        Creates a new population of individuals, a matrix of size population_size x num_cities.
-        Each row of the matrix corresponds to an individual.
-    :param num_cities:
-    :param population_size:
-    :return:
-    """
+class Population:
 
-    cities = np.empty(shape=(population_size, num_cities), dtype=int)
+    def __init__(self):
 
-    for i in range(population_size):
+        individuals = np.empty(shape=(population_size, size))
 
-        cities[i, :] = np.random.choice(num_cities, num_cities, replace=False) + 1
+        for i in range(population_size):
 
-    return cities
+            individuals[i, :] = Individual()
 
+        self.individuals = individuals
 
-def crossover(parents):
+    def crossover(self, parents):
 
-    assert (len(parents) == 2)
+        assert (len(parents) == 2)
 
-    length = len(parents[0])
-    points = [np.random.randint(0, length, dtype=int) for x in range(2)]
-    low, high = min(points), max(points)
+        length = len(parents[0])
+        points = [np.random.randint(0, length, dtype=int) for x in range(2)]
+        low, high = min(points), max(points)
 
-    children = np.empty((2, length), dtype=int)
+        children = np.empty((2, length), dtype=int)
 
-    for i, parent in enumerate(parents):
+        for i, parent in enumerate(parents):
+            other_parent = (parents[0] if i == 1 else parents[1])
+            middle = parent[low:high]
+            remaining = np.setdiff1d(other_parent, middle, assume_unique=True)
 
-        other_parent = (parents[0] if i == 1 else parents[1])
-        middle = parent[low:high]
-        remaining = np.setdiff1d(other_parent, middle, assume_unique=True)
+            end = remaining[:length - high]
+            start = remaining[length - high:]
 
-        end = remaining[:length - high]
-        start = remaining[length - high:]
+            children[i] = np.concatenate((start, middle, end))
 
-        children[i] = np.concatenate((start, middle, end))
-
-    return children
+        return children
 
 
-
-
-def mutate(individual):
-    """
-    Mutates a single individual using the displacement mutation (DM) operator. DM was used
-    because it was found to be the best mutation operator tested by Larranaga and Kuijpers.
-    :param individual: a NumPy array
-    :return:
-    """
-
-    points = [np.random.randint(0, len(individual), dtype=int) for x in range(2)]
-    low, high = min(points), max(points)
-
-    mutation = individual[low: high]
-    chromosome = np.concatenate((individual[:low], individual[high:]))
-    insertion_point = np.random.randint(0, len(chromosome), dtype=int)
-
-    return np.concatenate((chromosome[:insertion_point], mutation, chromosome[insertion_point:]))
-
-
-population = create_population(8, 2)
-children = crossover(population)
-
-print(population)
-print(children)
-
+population = Population()
 
 
 
